81f3ce81a27a121bbed5afbb091142683d9c30cc
Ahmed <ahmedkhalrd607@gmail.com>
2025-07-28 20:55:27 +0300

    creat errors folder

--- lib/features/Home/data/repos/home_repo.dart
+++ lib/features/Home/data/repos/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetshBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetshBestSellerBooks();
   fetchFeaturedooks();
 }
