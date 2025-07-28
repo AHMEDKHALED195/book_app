943d34f1724dbe3eafa19dd282b837730cbc9cc1
Ahmed <ahmedkhalrd607@gmail.com>
2025-07-29 02:06:35 +0300

    implement failur

--- lib/features/Home/data/repos/home_repo_impl.dart
+++ lib/features/Home/data/repos/home_repo_impl.dart
@@ -1,18 +1,52 @@
 import 'package:book_app/core/errors/failures.dart';
+import 'package:book_app/core/utlis/api_service.dart';
 import 'package:book_app/features/Home/data/models/book_model/book_model.dart';
 import 'package:book_app/features/Home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoIPmpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoIPmpl({required this.apiService});
   @override
-  fetchFeaturedooks() {
-    // TODO: implement fetchFeaturedooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetshNewsetSellerBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&Sorting=newest &q=computer science',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetshBestSellerBooks() {
-    // TODO: implement fetshBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=computer science',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+
+      return left(ServerFailure(e.toString()));
+    }
   }
 }
